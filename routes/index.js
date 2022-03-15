d1a075cb7c9c219146f29245aa9945f7f710f31a
shaymaalkhafaf <shaymaalkhafaf@gmail.com>
2022-03-15 18:05:59 -0500

    add comment route and database for it

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 require('dotenv').config();
 const isValidToken = require('../middleware/isValidToken')
 const {User} = require('../models');
+const comments = require('../models/comments');
+const { use } = require('./users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,6 +23,10 @@ router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Express' });
 });
 
+//comments route
+router.get('/comment',  function(req, res, next) {
+  res.render('comment');
+});
 
 
 router.get('/profile/:id', isValidToken, async function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,10 @@ var router = express.Router();
 const bcrypt = require("bcrypt");
 const saltRounds = bcrypt.genSaltSync(Number(process.env.SALT_FACTOR))
 const Sequelize = require('sequelize');
-const { User } = require('../models');
+const { User, Comments } = require('../models');
+
 const db = require("../models");
 const jwt = require('jsonwebtoken');
-const { user } = require('pg/lib/defaults');
 
 
 
@@ -66,6 +66,17 @@ router.post('/login', async (req, res, next) => {
 });
 
 
+//post a comment
+router.post('/comment', async (req, res, next) => {
+  let { name, title} = req.body;
+  console.log(name, title);
+
+  const newUser = await Comments.create({
+    name,
+    title
+  });
+  res.json(newUser);
+});
 
 
 
