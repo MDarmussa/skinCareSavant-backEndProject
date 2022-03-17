82c7160ed868ed36f27528b0416d861fa24ba19f
rashida-otunba <rashidaotunba@gmail.com>
2022-03-17 19:24:08 -0400

    first push; update to users

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ const axios = require('axios');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
 });
 
 /* POST users Register. */
